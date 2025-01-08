cc4e6fb0c3477e2ee3a9d18436278c7dfab88639
athu <athulyas2024@gmail.com>
2025-01-08 21:59:46 +0530

    created a employee management application and filtered the employees above 45

--- server.js
+++ server.js
@@ -4,36 +4,36 @@ import mongoose from 'mongoose';
 const app = express();
 
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 const url = 'mongodb://127.0.0.1:27017/employeeData';
 
-mongoose.connect(url).then(()=>{
+mongoose.connect(url).then(() => {
     console.log('Connected to MongoDB');
-    app.listen(3000, ()=>{
+    app.listen(3000, () => {
         console.log('Server is running on port 3000');
     })
-}).catch((err)=>{
+}).catch((err) => {
     console.log(err, "Error connecting to MongoDB");
 })
 
-const employeeSchema =  new mongoose.Schema({
-    name: {type: String, required: true},
-    age: {type: Number, required: true},
-    salary: {type: Number, required: true}
+const employeeSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    age: { type: Number, required: true },
+    salary: { type: Number, required: true }
 })
 
 const Employee = mongoose.model('Employee', employeeSchema);
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.sendFile('index.html');
     // const data = req.body;
 })
 
 
-app.post('/add', async(req, res)=>{
+app.post('/add', async (req, res) => {
     const data = req.body;
     console.log(data, "Employee Data");
     const employee = new Employee(data);
@@ -42,10 +42,22 @@ app.post('/add', async(req, res)=>{
 });
 
 
-app.get('/data', async(req, res)=>{
+app.get('/data', async (req, res) => {
     const data = await Employee.find();
-
-    console.log(data, "employees")
-    res.render('index.ejs', {data});
+    res.render('index.ejs', { data });
 
 })
+
+app.post('/edit-or-dlt', async (req, res) => {
+    const { id, name, age, salary, action } = req.body;
+
+    if (action === 'edit') {
+        await Employee.findByIdAndUpdate(id, { name, age, salary }, { new: true });
+        res.redirect('/data');
+    } else if (action === 'delete') {
+        await Employee.findByIdAndDelete(id);
+        res.redirect('/data');
+    } else {
+        res.status(400).send("Invalid action");
+    }
+});
